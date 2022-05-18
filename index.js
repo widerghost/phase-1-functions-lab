172500c279de339b518c0593a927bc0127f07424
Harlhey Wider Grammont <widergrammont@gmail.com>
2022-05-18 19:49:59 -0400

    commit tree 780b62abc68b7d9feff04fdb9d1c1de9be70dc5f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const hqLocation = '50';
+function distanceFromHqInBlocks(){
+    const pickupLocation = '49';
+    return (hqLocation - pickupLocation);
+}
+
+function distanceFromHqInBlocks(){
+    const pickupLocation = '42';
+    return (hqLocation - pickupLocation);
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(){
+    const pickupLocation = '49';
+    return ((hqLocation - pickupLocation) * 264)
+}
+
+function distanceFromHqInFeet(){
+    const pickupLocation = '42';
+    return ((hqLocation - pickupLocation) * 264);
+}
+
+
+function distanceTravelledInFeet(){
+    const tripStart = 48
+    const tripFinish = 43
+    return (tripStart - tripFinish) * 264
+}
+
+function distanceFromTravelledInFeet(){
+    const tripStart = 60
+    const tripFinish = 50
+    return (tripStart - tripFinish) * 264
+}
